0bff3093a001bec29b77c88902635b4caa55c314
armi-2077 <armigames109@gmail.com>
2024-01-21 10:02:55 -0600

    Updating styles and adding footer

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ async function getMonsters () {
     const kaijuCardTemplate = `
     <div class="Godzilla">
       <img id="img" src="${monsterElement.img}" width="500px" height="200px" alt="monster preview"/>
-      <h3>${monsterElement.name}</h3>
-      <h4>${monsterElement.firstAppearance}</h4>
+      <h3 id="flex-item-1">${monsterElement.name}</h3>
+      <h4 >${monsterElement.firstAppearance}</h4>
       ${monsterElement.ability}
     </div>
     `
